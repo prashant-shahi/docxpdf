cca266bf163da05c7a226e0225821c6c7dd661e3
Prashant Shahi <me@prashantshahi.dev>
2026-07-30 02:15:09 +0545

    feat: surface open source on the website
    Signed-off-by: Prashant Shahi <me@prashantshahi.dev>

---
+++ apps/web/src/lib/core/site.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2026 Prashant Shahi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** Public OSS repository (Apache-2.0). */
+export const GITHUB_REPO_URL = "https://github.com/prashant-shahi/docxpdf";
